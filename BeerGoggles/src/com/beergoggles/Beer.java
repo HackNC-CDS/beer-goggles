ac80fba8fb413965a97ddd88d2c29be62bb65bc2
Dave Perra <dpperraj@gmail.com>
2014-03-01 21:17:41 -0800

    update beer.

--- BeerGoggles/src/com/beergoggles/Beer.java
+++ BeerGoggles/src/com/beergoggles/Beer.java
@@ -1,5 +1,17 @@
 package com.beergoggles;
 
 public class Beer {
-
+	private final String name = "";
+	private double abv = 0.0;
+	private double rating = 0.0;
+	
+	public String name() {
+		return this.name;
+	}
+	public double rating() {
+		return this.rating;
+	}
+	public double abv() {
+		return this.abv;
+	}
 }
