a14e1a7931bf9e7775ba12a2708b044e2a7a1778
Dave Perra <dpperraj@gmail.com>
2014-03-01 21:32:39 -0800

    Fix null reference to array list.

--- BeerGoggles/src/com/beergoggles/MainActivity.java
+++ BeerGoggles/src/com/beergoggles/MainActivity.java
@@ -1,10 +1,7 @@
 package com.beergoggles;
 
-<<<<<<< HEAD
-=======
 import java.util.ArrayList;
 
->>>>>>> d0d17e3f04afce914217ec0dc891c9d2275fe01c
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -14,11 +11,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-<<<<<<< HEAD
+	public ArrayList<String> upcs = new ArrayList<String>();	
 	
-=======
-	public ArrayList<String> upcs;	
->>>>>>> d0d17e3f04afce914217ec0dc891c9d2275fe01c
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,39 +27,33 @@ public class MainActivity extends Activity {
     }
     
     public void FindBarcodes(View view) {
-//        Intent intent = new Intent("com.google.zxing.client.android.SCAN");
-//        intent.setPackage("com.google.zxing.client.android");
-//        intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
-//        startActivityForResult(intent, 0);
-//    	
     	IntentIntegrator integrator = new IntentIntegrator(this);
     	integrator.initiateScan();
     	
     	TextView textview = (TextView) findViewById(R.id.TopText);
     	textview.setText("Found barcodes.");
     }
-    
     public void RateBeers(View view) {
     	TextView textview = (TextView) findViewById(R.id.TopText);
     	textview.setText("Beers rated.");
     }
-    
     public void Visualize(View view) {
     	TextView textview = (TextView) findViewById(R.id.TopText);
     	textview.setText("Visualizing...");
     }
     
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-    	Toast t = Toast.makeText(this, "GOT IT", Toast.LENGTH_LONG);
+    	Toast t = Toast.makeText(this, "GOT IT", Toast.LENGTH_SHORT);
+    	t.show();
     	IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
     	if (scanResult != null) {
     		upcs.add(scanResult.getContents());
-			TextView textview = (TextView) findViewById(R.id.TopText);
-			String u = "";
-    		for(String i : upcs) {
+    		String u = "";
+    		for (String i : upcs) {
     			u += i + ", ";
     		}
-			textview.setText(scanResult.getContents());
+    		TextView textview = (TextView) findViewById(R.id.TopText);
+        	textview.setText(u);
     	}
     }
     
