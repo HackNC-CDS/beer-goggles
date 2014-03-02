9f97eab9ee2e21a3db8baf2e6485bd4e0c33db6f
Clinton Freeman <freemancw@appstate.edu>
2014-03-02 07:31:14 -0500

    dd

--- BeerGoggles/src/com/beergoggles/MainActivity.java
+++ BeerGoggles/src/com/beergoggles/MainActivity.java
@@ -24,19 +24,36 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.google.gson.*;
 
 public class MainActivity extends Activity {
 	
-	private ArrayList<String> upcs = new ArrayList<String>();	
+	//private ArrayList<String> upcs = new ArrayList<String>();	
 	private ArrayList<Beer> beers = new ArrayList<Beer>();
+	private ArrayList<String> beer_strings = new ArrayList<String>();
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        ListView blView = (ListView) findViewById(R.id.beer_list);
+
+        beer_strings.add("test beer");
+        
+        // This is the array adapter, it takes the context of the activity as a 
+        // first parameter, the type of list view as a second parameter and your 
+        // array as a third parameter.
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this, 
+                R.id.beer_list,
+                beer_strings );
+
+        blView.setAdapter(arrayAdapter); 
     }
 
     @Override
@@ -53,27 +70,19 @@ public class MainActivity extends Activity {
     
     public void onActivityResult(int requestCode, int resultCode, 
     		Intent intent) {
-    	Toast.makeText(this, "GOT IT", Toast.LENGTH_SHORT).show();
-    	IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
-    	if (scanResult != null) {
-    		upcs.add(scanResult.getContents());
-    		String u = "";
-    		for (String i : upcs) {
-    			u += i + ", ";
-    		}
-    	}
-    }
-    
-    public void RateBeers(View view) {
-    	for (String i : upcs) {
-    		Beer beer = FindBeerFromUPC(i);
-    		UpdateAndDisplayBeerName(beer.ba_rating);
+    	//Toast.makeText(this, "GOT IT", Toast.LENGTH_SHORT).show();
+    	IntentResult iresult = IntentIntegrator.parseActivityResult(requestCode, 
+    			resultCode, intent);
+    	if (iresult != null) {
+    		Beer beer = getBeerFromUPC(iresult.getContents()); 
     		beers.add(beer);
-    		// Do something with the beer
+    		beer_strings.add(beer.beer_name);
+    		
+    		//upcs.add(iresult.getContents());
     	}
     }
     
-    private Beer FindBeerFromUPC(String upc) {
+    public Beer getBeerFromUPC(String upc) {
     	RequestTask rt = new RequestTask();
     	rt.execute("http://www.upcdatabase.com/item/" + upc);
     	String beerName = "";
@@ -92,7 +101,7 @@ public class MainActivity extends Activity {
         HttpResponse response;
         String responseString = "";
         try {
-            response = httpclient.execute(new HttpGet("http://beergoggles-freemancw.rhcloud.com/beer/"+beerName));
+            response = httpclient.execute(new HttpGet("http://beergoggles-freemancw.rhcloud.com/beerSearch/"+beerName));
             StatusLine statusLine = response.getStatusLine();
             if (statusLine.getStatusCode() == HttpStatus.SC_OK) {
                 ByteArrayOutputStream out = new ByteArrayOutputStream();
@@ -111,35 +120,7 @@ public class MainActivity extends Activity {
         }
 
         Gson gson = new Gson();
-        return gson.fromJson(responseString, Beer.class);
-       
-        /*
-        class BagOfPrimitives {
-        	  private int value1 = 1;
-        	  private String value2 = "abc";
-        	  private transient int value3 = 3;
-        	  BagOfPrimitives() {
-        	    // no-args constructor
-        	  }
-        	}
-
-        	BagOfPrimitives obj = new BagOfPrimitives();
-        	
-        	String json = gson.toJson(obj);  
-        	==> json is {"value1":1,"value2":"abc"}
-
-        	Note that you can not serialize objects with circular references since that will result in infinite recursion. 
-
-        	(Deserialization)
-        	BagOfPrimitives obj2 = gson.fromJson(json, BagOfPrimitives.class);
-        	*/
-    	
-    	//return null;
-    }
-    
-    public void UpdateAndDisplayBeerName(String s) {
-		//TextView textview = (TextView) findViewById(R.id.TopText);
-    	//textview.setText(s);
+        return gson.fromJson(responseString, Beer.class); 
     }
     
 }
